40646d2ce5dfd36f881b5cc549b617db44c97a7d
Pixel4Inch <77658911+pixel4inch@users.noreply.github.com>
2025-08-12 17:26:21 +0530

    gsap text effects

---
+++ GsapProjects/01_SplitText/script.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+      // register plugin (note capital P)
+      gsap.registerPlugin(SplitText);
+
+      // create SplitText instance
+      const split = new SplitText(".box1-text", { 
+        type: "chars, words, lines",
+        charsClass: "chars",
+        //  wordsClass: "words",
+        //  linesClass: "lines", 
+         
+    });
+
+      // animate words
+      gsap.from(split.lines, {
+        rotation: 90,
+        tranformZ: 1000,
+        duration:0.8,
+        perspective: 800,
+        delay: 0.5,
+        opacity: 0,
+        yoyo: true,
+        repeat: -1,
+        stagger:0.2,
+        ease: "power2.inOut"
+      });
+    });
